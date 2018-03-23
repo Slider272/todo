9f0903a28877209c8f90f519f3def0c0e2d62048
Yakushin_yv <yakushin_yv@open.ru>
2018-03-23 12:42:05 +1000

    [todo] - Do better visual todo list - Add some visual CSS

--- src/toDoList.js
+++ src/toDoList.js
@@ -6,42 +6,86 @@ import {
   TableHeaderColumn,
   TableBody,
   TableRowColumn,
-  Checkbox
+  Checkbox,
 } from 'material-ui';
+import TextField from 'material-ui/TextField';
 
 class TodoList extends Component {
 
   render() {
     return (
       <div className='todo'>
-        <Table>
-          <TableHeader>
+        <Table
+          multiSelectable={true}
+        >
+          <TableHeader className='tableHeader'>
             <TableRow>
+
+              <TableHeaderColumn
+                style={{
+                  width: '10px',
+                }}
+              >
+                ID
+              </TableHeaderColumn>
+
               <TableHeaderColumn>
                 Title
               </TableHeaderColumn>
+
               <TableHeaderColumn>
                 Task
               </TableHeaderColumn>
-              <TableHeaderColumn>
+
+              <TableHeaderColumn
+                style={{
+                  width: '90px',
+                }}
+              >
                 Complited
               </TableHeaderColumn>
             </TableRow>
           </TableHeader>
           <TableBody>
-          {this.props.items.map(item => (
-            <TableRow key={item.id}>
-              <TableRowColumn>
-                {item.titles}
+          {this.props.items.map((item, index) => (
+            <TableRow key={item.id} className='todoRow'>
+              <TableRowColumn
+                style={{
+                  width: '10px',
+                }}
+              >
+                {index+1}
               </TableRowColumn>
               <TableRowColumn>
-                {item.task}
+                <TextField
+                  name={`${item.id}`}
+                  value={item.titles}
+                  disabled={true}
+                />
               </TableRowColumn>
               <TableRowColumn>
+                <TextField
+                  name={`${item.id}`}
+                  value={item.task}
+                  disabled={true}
+                  rows={2}
+                />
+              </TableRowColumn>
+              <TableRowColumn
+                style={{
+                  width: '90px',
+                }}
+              >
                 <Checkbox
                   checked={item.isComplited}
                   disabled={true}
                 />
+              </TableRowColumn>
+              <TableRowColumn
+                style={{
+                  width: '30px',
+                }}
+              >
               </TableRowColumn>
             </TableRow>
           ))}
